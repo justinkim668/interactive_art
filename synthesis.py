feaeb04233e4e19ee7722152f1a9f405f242f575
Justin Kim <jtk2141@columbia.edu>
2022-03-10 07:15:49 -0500

    audio synthesis fix

--- synthesis.py
+++ synthesis.py
@@ -12,26 +12,18 @@ while(True):
     json_data = json.loads(json_input)
     freq = json_data['potValue']/4
     mod = 0
+
     if (json_data['joyValSW'] == 0):
         a = SuperSaw(freq = freq).out()
     else:
-        
-    def determineType(voltage1, voltage2):
+        if (json_data['joyValX'] == 0):
+            mod = 0
+        elif(json_data['joyValX'] == 4056):
+            mod = 3
+        elif(json_data['joyValY'] == 0):
+            mod = 1
+        elif(json_data['joyValY'] == 4056):
+            mod = 2
 
-    if (json_data['joyValX'] <= 4056 and json_data['joyValX'] > 2028):
-        mod = 0
-    else:
-        mod = 3
-
-    if (json_data['joyValY'] <= 4056 and json_data['joyValY'] > 2028):
-        mod = 1
-    else:
-        mod = 2
-
-    if (json_data['joyValY'] <= 4056 and json_data['joyValY'] > 2028):
-        mod = 1
-    else:
-        mod = 2
-
-
-    a = LFO(freq = freq, type = mod).out()
+        a = LFO(freq = freq, type = mod).out()
+    print(json_data)
